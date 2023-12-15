22682440e16747150f5f95043fe28a23f73d0ad5
HongMinYeong <minyung1240@daum.net>
2023-12-15 15:52:01 +0900

    fix: StatisticsRepository 수정

--- src/main/java/back/ahwhew/repository/StatisticsRepository.java
+++ src/main/java/back/ahwhew/repository/StatisticsRepository.java
@@ -4,6 +4,14 @@ import back.ahwhew.entity.StatisticsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StatisticsRepository extends JpaRepository<StatisticsEntity,Long> {
+    // 테이블에 매핑될 엔티티 클래스는 statisticsEntity고 PK는 Long 타입
+    List<StatisticsEntity> findAll();
+    List<StatisticsEntity> findByAge(String age);
+    List<StatisticsEntity> findByGender(char gender);
+
 }
+
