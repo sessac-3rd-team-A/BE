a1f88890d5314dd0b7e28541dbabf24c10f85e49
HongMinYeong <minyung1240@daum.net>
2023-12-15 16:16:04 +0900

    fix: StatisticsRepository 수정 && StatisticsDTO 수정

--- src/main/java/back/ahwhew/repository/StatisticsRepository.java
+++ src/main/java/back/ahwhew/repository/StatisticsRepository.java
@@ -9,9 +9,14 @@ import java.util.List;
 @Repository
 public interface StatisticsRepository extends JpaRepository<StatisticsEntity,Long> {
     // 테이블에 매핑될 엔티티 클래스는 statisticsEntity고 PK는 Long 타입
+    StatisticsEntity findById(long id);
     List<StatisticsEntity> findAll();
+
     List<StatisticsEntity> findByAge(String age);
     List<StatisticsEntity> findByGender(char gender);
+
+    List<StatisticsEntity> findByDate(String date);
+
 
 }
 
--- src/main/java/back/ahwhew/service/StatisticsService.java
+++ src/main/java/back/ahwhew/service/StatisticsService.java
@@ -1,9 +1,42 @@
 package back.ahwhew.service;
 
+import back.ahwhew.entity.StatisticsEntity;
+import back.ahwhew.repository.StatisticsRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class StatisticsService {
+    // 필드 주입
+    @Autowired
+    private  StatisticsRepository repository;
+
+    // create statistics
+    public List<StatisticsEntity> create(final StatisticsEntity entity){
+    // read-only로 엔티티 설정 ㅇ_<
+        repository.save(entity);
+        log.info("Entity id: {} is saved. 통계데이터 추가 완료",entity.getId()); //통계데이터 생성시 로그
+
+        return repository.findAll();
+}
+    // read statics -> 우선 전체 읽어오기
+//    public List<StatisticsEntity> retrieveId(final String id) {
+//        return repository.findById(id);
+//    }
+
+    public List<StatisticsEntity> retrieveAge(final String age) {
+        return repository.findByAge(age);
+    }
+
+    public List<StatisticsEntity> retrieveGender(final char gender) {
+        return repository.findByGender(gender);
+    }
+
+    public List<StatisticsEntity> retrieveDate(final String date) {
+        return repository.findByDate(date);
+    }
 }
