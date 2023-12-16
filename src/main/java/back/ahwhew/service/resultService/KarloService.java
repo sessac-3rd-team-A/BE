ce3df8033de8d6e211dc9c7c006a9fe4006930d7
kimseeun <cocobell3@naver.com>
2023-12-16 22:21:59 +0900

    feat: 카를로 이미지 값 받아옴

--- src/main/java/back/ahwhew/service/resultService/KarloService.java
+++ src/main/java/back/ahwhew/service/resultService/KarloService.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -28,27 +29,34 @@ public class KarloService {
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("Authorization", karloKey);
+            headers.set("Authorization", "KakaoAK " + karloKey);
 
             RestTemplate restTemplate = new RestTemplate();
 
-            // Jackson ObjectMapper를 사용하여 JSON 요청 본문 생성
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, List<String>> requestBodyMap = new HashMap<>();
-            requestBodyMap.put("prompt", transferedWords);
-            String requestBody = objectMapper.writeValueAsString(requestBodyMap);
+            // 문자열로 이어서 JSON 요청 본문 생성
+            String requestBody = "{\"prompt\": \"" + String.join(",", transferedWords) + "\"}";
 
             HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-            log.info("karlo API에 보낼 요청 entit::{}", entity);
+            log.info("Karlo API에 보낼 요청 entity: {}", entity);
 
             // Karlo API 엔드포인트로 POST 요청 보내기
-            String result = restTemplate.postForObject(karloUrl, entity, String.class);
+            ResponseEntity<String> response = restTemplate.exchange(
+                    karloUrl,
+                    HttpMethod.POST,
+                    entity,
+                    String.class
+            );
 
-            // 결과 처리
-            processKarloResult(result);
-        } catch (JsonProcessingException e) {
-            log.error("JSON 처리 오류: {}", e.getMessage());
-        } catch (Exception e) {
+            if (response.getStatusCode() == HttpStatus.OK) {
+                // 결과 처리
+                processKarloResult(response.getBody());
+            } else {
+                // API 요청이 실패한 경우
+                log.error("Karlo API 요청 실패. 응답 코드: {}", response.getStatusCodeValue());
+            }
+        } catch (HttpClientErrorException e) {
+            log.error("Karlo API 요청 오류: {} - {}", e.getRawStatusCode(), e.getResponseBodyAsString());
+        } catch (RestClientException e) {
             log.error("Karlo API 요청 오류: {}", e.getMessage());
         }
     }
