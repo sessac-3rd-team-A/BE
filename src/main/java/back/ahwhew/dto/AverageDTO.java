5648651e73529072b63edc9db84efbeb6fd6a2a8
HongMinYeong <minyung1240@daum.net>
2023-12-20 15:44:22 +0900

    feat: averageDTO 에 date값 추가 /#70

--- src/main/java/back/ahwhew/dto/AverageDTO.java
+++ src/main/java/back/ahwhew/dto/AverageDTO.java
@@ -1,17 +1,22 @@
 package back.ahwhew.dto;
 
+import java.time.LocalDate;
+
 public class AverageDTO {
 
+    private LocalDate date;
     private double averagePositive;
     private double averageNegative;
     private double averageNeutral;
 
-    public AverageDTO(double averagePositive, double averageNegative, double averageNeutral) {
+    public AverageDTO(LocalDate date,double averagePositive, double averageNegative, double averageNeutral) {
+        this.date = date;
         this.averagePositive = averagePositive;
         this.averageNegative = averageNegative;
         this.averageNeutral = averageNeutral;
     }
 
+    public LocalDate getDate(){return date;}
     public double getAveragePositive() {
         return averagePositive;
     }
