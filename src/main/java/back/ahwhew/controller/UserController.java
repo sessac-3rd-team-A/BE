bb7839a42556f71ef1042c5a9463adfeac88cfc7
pipi-shortstocking <jannie435@naver.com>
2023-12-22 14:33:46 +0900

    fix: 회원가입 결과 응답값 수정(#86)

--- src/main/java/back/ahwhew/controller/UserController.java
+++ src/main/java/back/ahwhew/controller/UserController.java
@@ -55,14 +55,12 @@ public class UserController {
             // JsonNode를 String으로 변환
             String firstWord = wordsNode.get(0).asText();
 
+            log.info("nickname: " + firstWord);
+
             // 유저 유효성 검사
             String validCheck = isValidUser(dto);
             if (!validCheck.equals("checked")){
-                ResponseDTO resDTO = ResponseDTO.builder()
-                        .error(validCheck)
-                        .build();
-
-                return ResponseEntity.badRequest().body(resDTO);
+                return ResponseEntity.badRequest().body(false);
             }
 
                 UserEntity user = UserEntity.builder()
@@ -75,20 +73,20 @@ public class UserController {
 
                 UserEntity registeredUser = service.create(user);
 
-                UserDTO resDTO = UserDTO.builder()
-//                        .id(registeredUser.getId())
-                        .userId(registeredUser.getUserId())
-                        .password(registeredUser.getPassword())
-                        .age(registeredUser.getAge())
-                        .nickname(registeredUser.getNickname())
-                        .gender(registeredUser.getGender())
-                        .build();
+            UserDTO.builder()
+//                    .id(registeredUser.getId())
+                    .userId(registeredUser.getUserId())
+                    .password(registeredUser.getPassword())
+                    .age(registeredUser.getAge())
+                    .nickname(registeredUser.getNickname())
+                    .gender(registeredUser.getGender())
+                    .build();
 
-                return ResponseEntity.ok().body(resDTO);
+            return ResponseEntity.ok().body(true);
         } catch(Exception e) {
-            ResponseDTO resDTO = ResponseDTO.builder().error(e.getMessage()).build();
-            return ResponseEntity.badRequest().body(resDTO);
+            return ResponseEntity.badRequest().body(false);
         }
+
     }
 
     @PostMapping("/signin")
