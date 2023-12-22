433578e40c737ea4c2146338285d6415dca3470a
HongMinYeong <minyung1240@daum.net>
2023-12-22 14:56:20 +0900

    feat: 전체 유저 기준 가장 많이 나온 짤 기준으로 응답  /#76

--- src/main/java/back/ahwhew/controller/StatisticController.java
+++ src/main/java/back/ahwhew/controller/StatisticController.java
@@ -52,27 +52,28 @@ public class StatisticController {
     }
 
     @GetMapping("/statistics/meme")
-    public String getMeme(@RequestParam(required = false) Character gender,
+    public ResponseEntity <String> getMeme(@RequestParam(required = false) Character gender,
                           @RequestParam(required = false) String age){
         try{
+            LocalDate endDate = LocalDate.now();
+            LocalDate startDate = endDate.minusMonths(1); //한달 전
+            String gifUrl = null;
             if (gender != null && age != null) {
-                log.info("params 값: {}, {}", gender, age);
-                result = statisticsService.getAveragesByGenderAndAge(gender, age, startDate, endDate);
+//                gifUrl = statisticsService.getMemeByGenderAndAge(gender, age, startDate, endDate);
             } else if (gender != null) {
-                result = statisticsService.getAveragesByGender(gender, startDate, endDate);
+//                gifUrl = statisticsService.getMemeByGender(gender, startDate, endDate);
             } else if (age != null) {
-                result = statisticsService.getAveragesByAge(age, startDate, endDate);
+//                gifUrl = statisticsService.getMemeByAge(age, startDate, endDate);
             } else {
                 // 카테고리 선택 안했으면 전체 유저 평균 데이터 전송
-                result = statisticsService.getOverallAverages(startDate, endDate);
+                gifUrl = statisticsService.getMemeByUser(startDate, endDate);
             }
 
-            return gifUrl;
+            return ResponseEntity.ok().body(gifUrl);
         }catch (Exception e) {
             log.error("에러 발생: {}", e.getMessage(), e);
             // 예외에 대한 적절한 응답을 클라이언트에게 반환하거나, 기본값이나 빈 결과를 반환하는 등의 처리를 수행할 수 있습니다.
-            return String.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-
     }
 }
--- src/main/java/back/ahwhew/service/StatisticsService.java
+++ src/main/java/back/ahwhew/service/StatisticsService.java
@@ -215,38 +215,84 @@ public class StatisticsService {
         return new ArrayList<>(dailyAveragesMap.values());
         }
 
+    // 전체 유저별 가장 많이 나온 meme
+    public String getMemeByUser(LocalDate startDate, LocalDate endDate){
+        // 기간 안에 데이터 가져옴
+        List<StatisticsEntity> allDateInRange = repository.findAllByDateBetween(startDate,endDate);
 
-    public String getMemeByGenderAndAge(Character gender, String age) {
-        try {
-            List<StatisticsEntity> genderAndAgeData = repository.findByGenderAndAge(gender, age);
+        List<StatisticsEntity> filteredData = allDateInRange.stream()
+                .filter(entity -> {
+                    LocalDate entityDate = entity.getDate();
+                    return entityDate != null &&
+                            (entityDate.isEqual(startDate) || (entityDate.isAfter(startDate) && !entityDate.isAfter(endDate.plusDays(1))));
+                })
+                .collect(Collectors.toList());
 
-            // recommende_gif 값을 기준으로 내림차순 정렬
-            genderAndAgeData.sort((a, b) -> Integer.compare(b.getRecommendeGif(), a.getRecommendeGif()));
-
-            // 정렬된 리스트에서 첫 번째 요소의 recommende_gif 값을 가져옴
-            if (!genderAndAgeData.isEmpty()) {
-                return genderAndAgeData.get(0).getRecommendeGif();
-            } else {
-                // 데이터가 없는 경우 처리
-                return "No data available";
-            }
-        } catch (Exception e) {
-            // 예외 처리
-            return "Error occurred";
+        // 각 recommend_Gif의 빈도를 세기 위한 맵 생성
+        Map<String, Integer> gifFrequencyMap = new HashMap<>();
+        for (StatisticsEntity entity : filteredData) {
+            String recommendGif = entity.getRecommendedGif();
+            gifFrequencyMap.put(recommendGif, gifFrequencyMap.getOrDefault(recommendGif, 0) + 1);
         }
+
+        // 빈도가 가장 높은 recommend_Gif 찾기
+        String mostFrequentGif = Collections.max(gifFrequencyMap.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+        // 가장 높은 빈도의 recommend_Gif가 여러 개인 경우 랜덤 선택
+        String currentGif = mostFrequentGif;   // effectively final 변수
+        List<String> mostFrequentGifs = gifFrequencyMap.entrySet().stream()
+                .filter(entry -> {
+                    return entry.getValue().equals(gifFrequencyMap.get(currentGif));
+                })
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+
+        if (mostFrequentGifs.size() > 1) {
+            // 겹치는 값이 있을 경우 랜덤 선택
+            Random random = new Random();
+            mostFrequentGif = mostFrequentGifs.get(random.nextInt(mostFrequentGifs.size()));
+        }
+
+        return mostFrequentGif;
+        }
+
     }
 
 
-    public String getMemeByGender(Character gender){
 
-    }
+// 성별, 나이대별 가장 많이 나온 meme
+//    public String getMemeByGenderAndAge(Character gender, String age, LocalDate startDate, LocalDate endDate) {
+//        try {
+//            List<StatisticsEntity> genderAndAgeData = repository.findByGenderAndAge(gender, age);
+//
+//            // recommende_gif 값을 기준으로 내림차순 정렬
+//            genderAndAgeData.sort((a, b) -> String.compare(b.getRecommendedGif(), a.getRecommendedGif()));
+//
+//            // 정렬된 리스트에서 첫 번째 요소의 recommende_gif 값을 가져옴
+//            if (!genderAndAgeData.isEmpty()) {
+//                return genderAndAgeData.get(0).getRecommendedGif();
+//            } else {
+//                // 데이터가 없는 경우 처리
+//                return "No data available";
+//            }
+//        } catch (Exception e) {
+//            // 예외 처리
+//            return "Error occurred";
+//        }
+//    }
 
-    public String getMemeByAge(String age){
+    // 성별 가장 많이 나온 meme
+//    public String getMemeByGender(Character gender,LocalDate startDate, LocalDate endDate){
+//
+//    }
 
-    }
+    // 나이대별 가장 많이 나온 meme
+//    public String getMemeByAge(String age,LocalDate startDate, LocalDate endDate){
+//
+//    }
 
 
 
 
-}
-x
+//}
